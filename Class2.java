d014703c230c6285b6c426f7afa7e9dc7c4a0f60
Corey Dunn <coreyd8899@gmail.com>
2024-09-04 20:22:01 -0500

    Hotfix for f3() method

--- Class2.java
+++ Class2.java
@@ -8,5 +8,15 @@ public class Class2 {
     private void f1() {
         // v1.0
     }
-
+    public void f2() {
+        System.out.println("Feature 2");
+    }
+    
+    public void f3() {
+        System.out.println("Feature 3");
+    }
+    public void f4() {
+        System.out.println("Feature 4");
+    }
+    
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -14,5 +14,15 @@ public class HelloWorld {
     private void f1() {
         // v1.0
     }
-
+    public void f2() {
+        System.out.println("Feature 2");
+    }
+    
+    public void f3() {
+        System.out.println("Feature 3");
+    }
+    public void f4() {
+        System.out.println("Feature 4");
+    }
+    
 }
